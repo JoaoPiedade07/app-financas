f1bd5843ed6416284f782ceccf0c7dff7854dbe3
João Piedade <joaoluislopespiedade07@gmail.com>
2025-03-11 21:53:13 +0000

    Update profile.tsx

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
 const Settings = () => {
     return (
         <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Profile Screen</Text>
-
+            <TouchableOpacity style={styles.profileIconContainer}>
+                <Image 
+                source={require('@/assets/images/logo.png')} 
+                style={styles.profileIcon} 
+            />
+            </TouchableOpacity>
         </View>
     );
 }; 
 
 const styles = StyleSheet.create ({
-
+    profileIconContainer: {
+        position: 'absolute',
+        top: 20, // Ajuste conforme sua margem superior
+        right: 20,
+        zIndex: 10,
+      },
+    profileIcon: {
+        width: 70,
+        height: 70,
+        borderRadius: 50,
+        borderWidth: 2,
+        borderColor: '#fff',
+      },
 });
 
 export default Settings;

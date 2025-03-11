a030398e10b69c0d70f9ffc4c4ff5eb3c10be4d9
João Piedade <joaoluislopespiedade07@gmail.com>
2025-03-11 11:19:52 +0000

    changes

--- app/(atividades)/criarAtividades.tsx
+++ app/(atividades)/criarAtividades.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from "react-native-paper";
 import { Link } from 'expo-router';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native';
+import { ThemedView } from '@/components/ThemedView';
+import { Calendar, DateData } from 'react-native-calendars';
 import { Ionicons } from '@expo/vector-icons';
 
 const Finances = () => {
+
+    const [open, setOpen] = useState(false); //Open and close the calendar
+
+    function handleOpen() {
+      setOpen(!open);
+    }
+
     return (
         <View>
             <Text style = { styles.title }>New Expense</Text>
             <Card style = { styles.card }>
                 <Card.Content>
                     <input type="number" />
+                    <Text style = { styles.title }>Recepies</Text>
+                    <Text style = { styles.title }>Expense</Text>
+                    <Text style = { styles.title }>Upcoming Bills</Text>
                 </Card.Content>
             </Card>
             <Text style = { styles.title }>Category</Text>
@@ -41,7 +53,33 @@ const Finances = () => {
                             <Text style={ styles.transactionName }>Date</Text>
                         </View>
                     </View>
-                    <Text style={ styles.transactionValue }>11/03/2025</Text>
+                    <TouchableOpacity onPress={handleOpen}>
+                        <Text style={styles.transactionValue}>Set Date</Text>
+
+                        <Modal
+                        animationType='slide'
+                        transparent={true}
+                        visible={open}>
+                            <View style={styles.centerView}>
+                            <View style={styles.modalView}>
+
+                            <ThemedView style={styles.calendarContainer}>
+                                <Calendar 
+                                onDayPress={(day: DateData) => console.log('Selected day:', day)}
+                                markedDates={{ 
+                                '25-02-2025': {selected: true, marked: true, selectedColor: 'blue'},
+                                }}
+                                />
+                            </ThemedView>
+
+                            <TouchableOpacity style={styles.button} onPress={handleOpen}>
+                                <Text style={styles.buttonText}>Close Calendar</Text>
+                            </TouchableOpacity>
+
+                            </View>
+                            </View>
+                        </Modal>
+                    </TouchableOpacity>
                 </View>
                 </Card.Content>
             </Card>
@@ -64,7 +102,7 @@ const styles = StyleSheet.create ({
     },
     buttonText: {
         color: '#fff',
-        fontSize: 30,
+        fontSize: 18,
         textAlign: 'center',
         
     },
@@ -87,7 +125,7 @@ const styles = StyleSheet.create ({
         justifyContent: 'center',
         alignItems: 'center',
         marginRight: 5,
-        marginLeft: 10, // Espaço entre o ícone e o texto
+        marginLeft: 0, // Espaço entre o ícone e o texto
     },
     transactionName: {
         fontSize: 16,
@@ -110,6 +148,43 @@ const styles = StyleSheet.create ({
         //fontWeight: 'bold',
         marginRight: 15,
     },
+    calendarContainer: {
+        padding: 10,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        elevation: 3,
+      },
+    centerView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+      },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        width: '80%',
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+    button: {
+        marginTop: 10,
+        height: 50,
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        backgroundColor: '#007bff',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
 })
 
 export default Finances;
--- app/(tabs)/finances.tsx
+++ app/(tabs)/finances.tsx
@@ -1,20 +1,84 @@
 import React from 'react';
 import { Link } from 'expo-router';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 
 const Finances = () => {
     return (
-        <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Finances Screen</Text>
+        <View style={styles.screen}>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <Text style={styles.title}>Transactions</Text>
+            <View style={styles.transactionRow}>
+                    <View style={styles.transactionInfo}>
+                        <View style={[styles.iconContainerWeak, { backgroundColor: '#4CAF50' }]}>
+                            <Ionicons name="arrow-up-outline" size={18} color="white" />
+                        </View>
+                        <View>
+                            <Text style={styles.transactionName}>App UI</Text>
+                            <Text style={styles.transactionDate}>12 Set 2025</Text>
+                        </View>
+                    </View>
+                    <Text style={[styles.transactionValue, styles.positive]}>+10.000</Text>
+                </View>
+                <View style={styles.transactionRow}>
+                    <View style={styles.transactionInfo}>
+                        <View style={[styles.iconContainerWeak, { backgroundColor: '#F44336' }]}>
+                            <Ionicons name="arrow-down-outline" size={18} color="white" />
+                        </View>
+                        <View>
+                            <Text style={styles.transactionName}>Food Shopping</Text>
+                            <Text style={styles.transactionDate}>23 Dec 2025</Text>
+                        </View>
+                    </View>
+                    <Text style={[styles.transactionValue, styles.negative]}>-30.45</Text>
+                </View>
+                <View style={styles.transactionRow}>
+                    <View style={styles.transactionInfo}>
+                        <View style={[styles.iconContainerWeak, { backgroundColor: '#F44336' }]}>
+                            <Ionicons name="arrow-down-outline" size={18} color="white" />
+                        </View>
+                        <View>
+                            <Text style={styles.transactionName}>Shopping</Text>
+                            <Text style={styles.transactionDate}>10 Set 2025</Text>
+                        </View>
+                    </View>
+                    <Text style={[styles.transactionValue, styles.negative]}>-120.99</Text>
+                </View>
+                <View style={styles.transactionRow}>
+                    <View style={styles.transactionInfo}>
+                        <View style={[styles.iconContainerWeak, { backgroundColor: '#4CAF50' }]}>
+                            <Ionicons name="arrow-up-outline" size={18} color="white" />
+                        </View>
+                        <View>
+                            <Text style={styles.transactionName}>Visual Design</Text>
+                            <Text style={styles.transactionDate}>08 Set 2025</Text>
+                        </View>
+                    </View>
+                    <Text style={[styles.transactionValue, styles.positive]}>+300.00</Text>
+                </View>
+            </ScrollView>
             {/* 🔹 Botão flutuante FIXO no fundo da tela */}
             <TouchableOpacity style={styles.createButton}>
                 <Link href={ '/(atividades)/criarAtividades' } style = { styles.buttonText }>+</Link>
             </TouchableOpacity>
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create ({
+    screen: {
+        flex: 1, // Ocupa toda a tela
+    },
+    scrollContainer: {
+        paddingBottom: 80, // Garante espaço para o botão
+    },
+    title: {
+        fontSize: 18,
+        marginLeft: 15,
+        marginTop: 10,
+        color: '#666',
+    },
     createButton: {
         position: 'absolute',
         bottom: 90,
@@ -32,6 +96,47 @@ const styles = StyleSheet.create ({
         fontSize: 30,
         textAlign: 'center',
         
+    },
+    transactionRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    transactionInfo: {
+        flexDirection: 'row', // Para alinhar ícone e texto lado a lado
+        alignItems: 'center', // Centraliza verticalmente
+    },
+    transactionName: {
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 4, // Espaçamento entre nome e data
+        marginLeft: 5,
+    },
+    transactionDate: {
+        fontSize: 12,
+        color: '#666',
+        marginLeft: 5,
+    },
+    transactionValue: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginRight: 15,
+    },
+    positive: {
+        color: '#4CAF50',
+    },
+    negative: {
+        color: '#F44336',
+    },
+    iconContainerWeak: {
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 5,
+        marginLeft: 10, // Espaço entre o ícone e o texto
     },
 })
 

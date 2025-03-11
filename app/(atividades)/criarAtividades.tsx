64dbdd4eeee5f7c1fb8a4a9cf5f25d7032c7d130
João Piedade <joaoluislopespiedade07@gmail.com>
2025-03-11 10:43:33 +0000

    adding categories, date and values

--- app/(atividades)/criarAtividades.tsx
+++ app/(atividades)/criarAtividades.tsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { Card } from "react-native-paper";
 import { Link } from 'expo-router';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const Finances = () => {
     return (
-        <View style = {{ flex: 1, alignItems: 'center' }}>
-            <Text>New Expense</Text>
-            <input type="number" />
-            <Text>Category</Text>
-            <Text>Date</Text>
+        <View>
+            <Text style = { styles.title }>New Expense</Text>
+            <Card style = { styles.card }>
+                <Card.Content>
+                    <input type="number" />
+                </Card.Content>
+            </Card>
+            <Text style = { styles.title }>Category</Text>
+            <Card style = { styles.card }>
+                <Card.Content>
+                <View style={styles.transactionRow}>
+                    <View style={styles.transactionInfo}>
+                        <View style={[styles.iconContainerWeak, { backgroundColor: '#4CAF50' }]}>
+                            <Ionicons name="pricetag-outline" size={18} color="white" />
+                        </View>
+                        <View>
+                            <Text style={styles.transactionName}>Category</Text>
+                        </View>
+                    </View>
+                    <Text style={ styles.transactionValue }>Home</Text>
+                </View>
+                </Card.Content>
+            </Card>
+            <Text style = { styles.title }>Date</Text>
+            <Card style = { styles.card }>
+                <Card.Content>
+                <View style={styles.transactionRow}>
+                    <View style={styles.transactionInfo}>
+                        <View style={[styles.iconContainerWeak, { backgroundColor: '#4CAF50' }]}>
+                            <Ionicons name="calendar-outline" size={18} color="white" />
+                        </View>
+                        <View>
+                            <Text style={ styles.transactionName }>Date</Text>
+                        </View>
+                    </View>
+                    <Text style={ styles.transactionValue }>11/03/2025</Text>
+                </View>
+                </Card.Content>
+            </Card>
         </View>
     )
 }
@@ -31,6 +67,48 @@ const styles = StyleSheet.create ({
         fontSize: 30,
         textAlign: 'center',
         
+    },
+    card: {
+        margin: 10,
+        borderRadius: 10,
+        elevation: 4,
+        backgroundColor: "white",
+    },
+    title: {
+        fontSize: 18,
+        marginLeft: 15,
+        marginTop: 10,
+        color: '#666',
+    },
+    iconContainerWeak: {
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 5,
+        marginLeft: 10, // Espaço entre o ícone e o texto
+    },
+    transactionName: {
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 4, // Espaçamento entre nome e data
+        marginLeft: 5,
+    },
+    transactionRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    transactionInfo: {
+        flexDirection: 'row', // Para alinhar ícone e texto lado a lado
+        alignItems: 'center', // Centraliza verticalmente
+    },
+    transactionValue: {
+        fontSize: 16,
+        //fontWeight: 'bold',
+        marginRight: 15,
     },
 })
 

453806961ddaf3425ad8c2fbd32301027007d2a3
João Piedade <joaoluislopespiedade07@gmail.com>
2025-03-12 12:48:41 +0000

    Update icon.tsx

--- constants/icon.tsx
+++ constants/icon.tsx
@@ -4,13 +4,14 @@ export const icon = {
     home: ( props: any ) => ( 
       <Feather name = 'home' size = { 24 } { ...props } /> 
     ),
-    list: ( props: any ) => ( 
+    finances: ( props: any ) => ( 
+      <Feather name = 'bar-chart' size = { 24 } { ...props } /> 
+    ),
+    budget: ( props: any ) => ( 
       <Feather name = 'list' size = { 24 } { ...props } /> 
     ),
     profile: ( props: any ) => ( 
       <Feather name = 'user' size = { 24 } { ...props } /> 
     ),
-    finances: ( props: any ) => ( 
-      <Feather name = 'bar-chart' size = { 24 } { ...props } /> 
-    ),
+    
 }

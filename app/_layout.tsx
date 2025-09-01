4f68e5685c926402d584b91330ab5470bb3f21f8
João Piedade <153988325+JoaoPiedade07@users.noreply.github.com>
2025-09-01 21:54:47 +0100

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { AuthProvider } from './(auth)/AuthContext';
 import { ThemeProvider } from '@/components/ThemeContext';
 import { TransactionProvider } from './Transactions/TransactionContent';
@@ -7,21 +8,23 @@ import { ImageProvider } from './Image/ImageContent';
 
 export default function RootLayout() {
   return (
-    <ThemeProvider>
-      <ImageProvider>
-        <LanguageProvider>
-          <AuthProvider>
-            <TransactionProvider>
-              <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                <Stack.Screen name="(signup)" options={{ headerShown: false }} />
-                <Stack.Screen name="(profile)" options={{ headerShown: false }} />
-                <Stack.Screen name="index" options={{ headerShown: false }} />
-              </Stack>
-            </TransactionProvider>
-          </AuthProvider>
-        </LanguageProvider>
-      </ImageProvider>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider>
+        <ImageProvider>
+          <LanguageProvider>
+            <AuthProvider>
+              <TransactionProvider>
+                <Stack>
+                  <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                  <Stack.Screen name="(signup)" options={{ headerShown: false }} />
+                  <Stack.Screen name="(profile)" options={{ headerShown: false }} />
+                  <Stack.Screen name="index" options={{ headerShown: false }} />
+                </Stack>
+              </TransactionProvider>
+            </AuthProvider>
+          </LanguageProvider>
+        </ImageProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
